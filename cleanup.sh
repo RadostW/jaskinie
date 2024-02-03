c1792d38f8f910baeb2b9be2c379db3e88cf3566
RadostW <radost.waszkiewicz@gmail.com>
2024-02-03 12:58:09 +0100

    WIP

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 find -name "*.err" | xargs -I {} rm {}
-find -not -path "./Docs/*" -name "*.3d" | xargs -I {} rm {}
+find -not -path "./Docs/*" -not -path "./Drawings/*" -name "*.3d" | xargs -I {} rm {}
 
